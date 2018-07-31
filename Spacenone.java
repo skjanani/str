9f84bde9af36061480754128e74a01924cdb2648
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-31 14:32:07 +0530

    Add files via upload

---
+++ Spacenone.java
@@ -0,0 +1,17 @@
+package janani;
+import java.util.Scanner;
+public class Spacenone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String n,n1;
+System.out.println("enter the string");
+Scanner ja=new Scanner(System.in);
+n=ja.next();
+n1=ja.next();
+String a=n+n1;
+//System.out.println(n.concat(n1));
+System.out.println(a);
+	}
+
+}
